740a530cfb34e7a8c22b5b8b59c61a19647fa868
Vinod Nandimandalam <vinodnandimandalam@gmail.com>
2025-03-22 22:16:12 +0530

    Adds https methods

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express()
 
-app.use("/test", (req, res) => {
-    res.send('hello hello hello')
+app.get("/test", (req, res) => {
+    res.send("hello world")
 })
+
+app.post("/test", (req, res) => {
+    console.log("test post request")
+    res.send("test post request")
+})
+
+app.put("/test", (req, res) => {
+    console.log("test put request")
+    res.send("test put request")
+})
+
+app.delete("/test", (req, res) => {
+    console.log("test delete request")
+    res.send("test delete request")
+})
+
 app.listen(3000, () => {
     console.log("server listening on 3000")
 });
